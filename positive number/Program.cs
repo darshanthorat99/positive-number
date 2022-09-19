a745459aa67bc326199061e109f5cdd216926c29
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-19 17:48:04 +0530

    Add project files.

---
+++ positive number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positive_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("number is positive");
+            }
+            else if(num < 0)
+            {
+                Console.WriteLine("number is negative");
+            }
+            else
+            {
+                Console.WriteLine("number is zero");
+            }
+        }
+    }
+}
